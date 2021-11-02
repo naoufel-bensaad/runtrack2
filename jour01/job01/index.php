bbb200162199eef0a55f3c55103d9d2b1735117a
Ben saad Naoufel <91779874+naoufel-bensaad@users.noreply.github.com>
2021-11-02 15:30:55 +0100

    Add files via upload
    j'ai créer des variable, Affectez des valeur, Affichez du contenu.

---
+++ jour01/job01/index.php
@@ -0,0 +1,26 @@
+<?php
+    $str = " LaPlateforme";
+    $str2 = " Vive";
+    $str3 = " !";
+    echo $str2.$str.$str3;
+    echo "<br>";
+
+
+
+    $val = 6;
+    echo $val ;
+    //$val =$val+4;
+    $val += 4;
+    echo $val ; 
+    echo "<br>";
+
+    
+    $myBool = true;
+    echo $myBool;
+    $myBool = false;
+    echo $myBool;
+
+
+?>
+
+
