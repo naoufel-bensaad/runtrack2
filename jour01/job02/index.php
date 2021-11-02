1f9940befd6e5ecc8e6d560c60e5d968f307740e
Ben saad Naoufel <91779874+naoufel-bensaad@users.noreply.github.com>
2021-11-02 15:38:52 +0100

    Add files via upload
    ajouter des commentaire

---
+++ jour01/job02/index.php
@@ -0,0 +1,13 @@
+<?php
+    $str = " LaPlateforme";
+    $str2 = " Vive";
+    $str3 = " !";
+    echo $str2.$str.$str3;//Première lignes
+    echo "<br>";
+    echo $str2.$str.$str3;//Deuxième lignes
+
+
+
+?>
+
+
