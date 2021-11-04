056edd2276d3610d3c1d365dd2f46ecffe091ac3
Ben saad Naoufel <91779874+naoufel-bensaad@users.noreply.github.com>
2021-11-04 13:52:31 +0100

    Add files via upload

---
+++ jour02/job01/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nombre = 0;
+    while ($nombre <1337){
+        $nombre += 1;
+        if ($nombre == 42) {
+            echo "<u><b>$nombre</b></u></br>";
+
+        } else {
+            echo"$nombre </br> ";
+        }
+    }
+?>
